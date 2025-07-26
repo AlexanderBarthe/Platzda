b9e4b36130ed103f16be2ab375672390bd3391b7
AlexanderBarthe <a.barthe@proton.me>
2025-07-26 19:29:00 +0200

    Initial

---
+++ src/main/java/com/eva/platzda/PlatzdaApplication.java
@@ -0,0 +1,13 @@
+package com.eva.platzda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatzdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatzdaApplication.class, args);
+	}
+
+}
