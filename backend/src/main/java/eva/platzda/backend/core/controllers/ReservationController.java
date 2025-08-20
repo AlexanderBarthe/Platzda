0f98784d360a40ff54ae8e902f97e6a42e757da5
Oskar Oetken <oskaroetken@web.de>
2025-08-21 00:59:35 +0200

    Add beginning for reservation creation

---
+++ backend/src/main/java/eva/platzda/backend/core/controllers/ReservationController.java
@@ -0,0 +1,11 @@
+package eva.platzda.backend.core.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reservation")
+public class ReservationController {
+
+
+}
---
+++ backend/src/main/java/eva/platzda/backend/core/services/ReservationService.java
@@ -0,0 +1,37 @@
+package eva.platzda.backend.core.services;
+
+
+import eva.platzda.backend.core.models.Timeslot;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ReservationService {
+
+    private final TableService tableService;
+    private final UserService userService;
+    private final RestaurantService restaurantService;
+    private final TimeslotService timeslotService;
+
+
+    public ReservationService(TableService tableService, UserService userService, RestaurantService restaurantService, TimeslotService timeslotService) {
+        this.tableService = tableService;
+        this.userService = userService;
+        this.restaurantService = restaurantService;
+        this.timeslotService = timeslotService;
+    }
+
+
+    public List<Timeslot> getFreeTimeslots(int persons, LocalDate date) {
+        List<Timeslot> freeSlotsGeneral = timeslotService.finAllFreeTimeslots();
+        List<Timeslot> freeSlotsDay = freeSlotsGeneral.stream()
+                .filter(slot -> slot.getStartTime().toLocalDate().equals(date))
+                .toList();
+        return null;
+
+    }
+
+}
